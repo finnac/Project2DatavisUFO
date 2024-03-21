51e275f0ff2730f900c02f1a63fe064b9d7e45ac
finnac <finnac@mail.uc.edu>
2024-03-21 13:54:33 -0400

    Prototype modal pop-up for settings

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 
 
+
+
 d3.csv('data/ufoSample.csv')
 .then(data => {
     console.log(data[0]);
@@ -24,6 +26,12 @@ d3.csv('data/ufoSample.csv')
 
     });
 
+    // Initialize chart and then show it
+    leafletMap = new LeafletMap({ parentElement: '#my-map'}, data);
+
+
+    
+
     //INITIAL MAP RENDERS
 
     //NEED FUNCTION TO RENDER NEW MAPS WHEN SETTINGS CHANGE
@@ -37,9 +45,29 @@ d3.csv('data/ufoSample.csv')
     // (End values inclusive)
 
 
-    // Initialize chart and then show it
-    leafletMap = new LeafletMap({ parentElement: '#my-map'}, data);
-
+    
 
   })
   .catch(error => console.error(error));
+
+    // Add an event listener to the settings button using D3
+    d3.select("#detailSettingsButton").on("click", function() {
+      // Show the modal when the button is clicked
+      document.getElementById("myModal").style.display = "block";
+    });
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      document.getElementById("myModal").style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      var modal = document.getElementById("myModal");
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+    }
